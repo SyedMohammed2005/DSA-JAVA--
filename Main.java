1457f6c5da32513d8d3504467c8f7ae9595fee0f
Syed mohammed pasha quadri <syedmdpashaquadri2005@gmail.com>
2026-09-17 20:06:22 +0530

    feat(java): add solution via CodeJ

--- Main.java
+++ Main.java
@@ -6,26 +6,17 @@ class CodeJ
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		 Scanner sc = new Scanner(System.in);
-        System.out.println("enter n");
-        int n = sc.nextInt();
-
-        for (int i = 0; i < n; i++) {
-            // space
-            for (int j = 0; j < n - i - 1; j++) {
-                System.out.print(" ");
-            }
-
-            // star
-            for (int j = 0; j < 2 * i + 1; j++) {
-                System.out.print("*");
-            }
-
-            // move to next line (INSIDE the outer loop)
-            System.out.println();
-        }
-
-        sc.close();
-
+		
+		int n = 7;
+		int a = 0;
+		int b = 1;
+		System.out.println("Fibonacci Series up to " + n + " terms:");
+		for (int i = 1; i <= n; i++) {
+			System.out.print(a + " ");
+			int next = a + b;
+			a = b;
+			b = next;
+		}
+		System.out.println();
 	}
 }
